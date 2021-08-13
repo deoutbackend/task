d66471a750d6580047fd30b745d0fa42e5d5fafc
TigranMelkonyan <tigran.melqonyan96@gmail.com>
2021-08-13 23:36:40 +0400

    Tigran Melkonyan

--- src/main/java/com/deout/task/IntIterable.java
+++ src/main/java/com/deout/task/IntIterable.java
@@ -3,8 +3,8 @@ package com.deout.task;
 import java.util.Iterator;
 
 public class IntIterable implements Iterable<Integer> {
-    int[] backed;
-
+    private int[] backed;
+    private int index = 0;
 
     public IntIterable(int[] backed) {
         this.backed = backed;
@@ -14,22 +14,43 @@ public class IntIterable implements Iterable<Integer> {
         return new IntIterator();
     }
 
-    private int index = 0 ;
-
     private class IntIterator implements Iterator<Integer> {
-
         public boolean hasNext() {
-            //TODO: Your task is implement this method
-            return false;
+            assertNotNull();
+            return index < backed.length;
         }
 
         public Integer next() {
-            //TODO: Your task is implement this method
-            return 0;
+            assertNotNull();
+            assertIndexNotOutOfRange();
+            return backed[index++];
         }
 
         public void remove() {
-            //TODO: Your task is implement this method
+            int[] arr = new int[backed.length - 1];
+            assertNotNull();
+            assertIndexNotOutOfRange();
+            for (int i = 0; i < backed.length; i++) {
+                if (index - 1 == i) {
+                    for (int j = i; j < backed.length - 1; j++) {
+                        arr[j] = backed[j + 1];
+                    }
+                    break;
+                }
+                arr[i] = backed[i];
+            }
+            backed = arr;
+        }
+    }
+
+    private void assertNotNull() {
+        if (backed == null) {
+            throw new NullPointerException("Array cant be null");
+        }
+    }
+
+    private void assertIndexNotOutOfRange() {
+        if (index < 0 || index >= backed.length) {
             throw new IllegalStateException("Could not remove from array");
         }
     }
