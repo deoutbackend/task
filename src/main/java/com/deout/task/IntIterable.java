4688b704a0b4d1f9dd6d70c301a9cd80a3a34462
SevoMkrtchyan <sevo-mkrtchyan@mail.ru>
2021-09-11 20:53:23 +0400

    implementation of hasNext(), next() and remove() methods v2

--- src/main/java/com/deout/task/IntIterable.java
+++ src/main/java/com/deout/task/IntIterable.java
@@ -20,22 +20,31 @@ public class IntIterable implements Iterable<Integer> {
 
         public boolean hasNext() {
             //TODO: Your task is implement this method
-            return index < backed.length;
-
+//            return index < backed.length;
+            for (int i = 0; i < backed.length; i++) {
+                if (i == index && ++i != 0) {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public Integer next() {
             //TODO: Your task is implement this method
-            return backed[index++];
+//            return backed[index++];
+            for (int i = 0; i < backed.length; i++) {
+                if (hasNext()) {
+                    return backed[index++];
+                }
+            }
+            return 0;
         }
 
         public void remove() {
             //TODO: Your task is implement this method
             for (int i = 0; i < backed.length; i++) {
                 if (backed[i] == index) {
-                    for (int j = i; j < backed.length - 1; j++) {
-                        backed[j] = backed[j + 1];
-                    }
+                    if (backed.length - 1 - i >= 0) System.arraycopy(backed, i + 1, backed, i, backed.length - 1 - i);
                     break;
                 }
             }
